6ee8909a360f97cb202c4be0a748c3f0b89bf16b
carolpan <carolpan@mit.edu>
2021-04-17 22:41:56 -0400

    resolving papapa merge conflicts

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import TaskList from './TaskList'
+import ProductivityDonutChart from './ProductivityDonutChart'
+import ProgressDonutChart from './ProgressDonutChart'
+
+const ProgressContext = React.createContext({checked: 0, unchecked: 1})
+const ProductivityContext = React.createContext({timeProductive: 1, timeUnproductive: 0})
+
+const tasks = [
+  {checked: false, value: "Click here to add to-dos!", shouldFocus: false}
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p> Hello</p>
-      </header>
+    <div>
+      <TaskList tasks={tasks} context={ProgressContext} />
+      <ProgressDonutChart context={ProgressContext}/>
+      
+      <ProductivityDonutChart context={ProductivityContext}/>
     </div>
   );
 }
